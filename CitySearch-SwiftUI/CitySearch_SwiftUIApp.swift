6c7722264e4a915d700fb523b2dd4f3e1f6993f2
Sebastiao Gazolla Costa Junior <gazolla@gmail.com>
2022-07-09 00:15:56 -0300

    Initial Commit

---
+++ CitySearch-SwiftUI/CitySearch_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySearch_SwiftUIApp.swift
+//  CitySearch-SwiftUI
+//
+//  Created by sebastiao Gazolla Costa Junior on 09/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CitySearch_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CitySearch-SwiftUI/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  CitySearch-SwiftUI
+//
+//  Created by sebastiao Gazolla Costa Junior on 09/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
